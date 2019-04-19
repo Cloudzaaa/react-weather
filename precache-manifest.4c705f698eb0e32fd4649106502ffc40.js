986c03b9d22c71de219a89c38ac32a501bb131fd
a.gavrilov <a.gavrilov@webvork.com>
2019-04-19 19:23:39 +0300

    Updates

---
+++ precache-manifest.4c705f698eb0e32fd4649106502ffc40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fde8c0d54ff418123e92",
+    "url": "/react-weather/static/js/runtime~main.fde8c0d5.js"
+  },
+  {
+    "revision": "3181ab89597ed5556e1b",
+    "url": "/react-weather/static/js/main.3181ab89.chunk.js"
+  },
+  {
+    "revision": "77d0e620c455f91bbfc4",
+    "url": "/react-weather/static/js/2.77d0e620.chunk.js"
+  },
+  {
+    "revision": "3181ab89597ed5556e1b",
+    "url": "/react-weather/static/css/main.dab7a048.chunk.css"
+  },
+  {
+    "revision": "77d0e620c455f91bbfc4",
+    "url": "/react-weather/static/css/2.2104678d.chunk.css"
+  },
+  {
+    "revision": "53141a9b2e147ccb841b3f5e12288f25",
+    "url": "/react-weather/index.html"
+  }
+];
